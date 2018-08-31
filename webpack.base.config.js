d2c085546a7f3d4a4117a8ff725e992fc7790bef
Perry Mitchell <perry@kiosked.com>
2018-08-31 09:03:35 +0300

    Split webpack configs

---
+++ webpack.base.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+const DIST = path.resolve(__dirname, "./dist");
+const SOURCE = path.resolve(__dirname, "./source");
+
+module.exports = {
+    entry: path.join(SOURCE, "index.js"),
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    },
+
+    output: {
+        filename: "has-consent.js",
+        path: DIST
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,4 @@
-const path = require("path");
-
-const DIST = path.resolve(__dirname, "./dist");
-const SOURCE = path.resolve(__dirname, "./source");
-
-const baseConfig = {
-    entry: path.join(SOURCE, "index.js"),
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            }
-        ]
-    },
-
-    output: {
-        filename: "has-consent.js",
-        path: DIST
-    }
-};
+const baseConfig = require("./webpack.base.config.js");
 
 module.exports = (env, argv) => {
     return argv.mode === "production"
