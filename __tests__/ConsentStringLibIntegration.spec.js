1e46654d8c0c563e3395b3e53ea6b01152901409
Perry Mitchell <perry@kiosked.com>
2018-09-04 09:37:38 +0300

    Add tests for consent-string integration

---
+++ __tests__/ConsentStringLibIntegration.spec.js
@@ -0,0 +1,31 @@
+const { ConsentString } = require("consent-string");
+const { ConsentStringParser } = require("../dist/has-consent.js");
+const globalVendorsList = require("./resources/vendorList.json");
+
+describe("Consent-String integration", function() {
+    it("detects allowed vendors", function() {
+        const consentData = new ConsentString();
+        consentData.setGlobalVendorList(globalVendorsList);
+        consentData.setCmpId(1);
+        consentData.setCmpVersion(1);
+        consentData.setVendorsAllowed([1, 24, 245]);
+        const consentString = consentData.getConsentString();
+        const parser = new ConsentStringParser(consentString);
+        expect(parser.vendorAllowed(1)).toBe(true);
+        expect(parser.vendorAllowed(24)).toBe(true);
+        expect(parser.vendorAllowed(245)).toBe(true);
+    });
+
+    it("detects disallowed vendors", function() {
+        const consentData = new ConsentString();
+        consentData.setGlobalVendorList(globalVendorsList);
+        consentData.setCmpId(1);
+        consentData.setCmpVersion(1);
+        consentData.setVendorsAllowed([1, 24, 245]);
+        const consentString = consentData.getConsentString();
+        const parser = new ConsentStringParser(consentString);
+        expect(parser.vendorAllowed(2)).toBe(false);
+        expect(parser.vendorAllowed(23)).toBe(false);
+        expect(parser.vendorAllowed(91)).toBe(false);
+    });
+});
