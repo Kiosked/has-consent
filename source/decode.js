c6ce510cdb1274d2de11598b325490b8ef53ece3
Perry Mitchell <perry@kiosked.com>
2018-09-04 09:35:52 +0300

    Add unsafe base64 preparation

--- source/decode.js
+++ source/decode.js
@@ -7,13 +7,24 @@ export function convertByteToBitstring(byte) {
 }
 
 export function decodeBase64ToArrayBuffer(b64, win = window) {
-    const binaryString = win.atob(b64);
+    const binaryString = win.atob(prepareBase64ForDecode(b64));
     const len = binaryString.length;
     const bytes = new Uint8Array(len);
     for (let i = 0; i < len; i += 1) {
         bytes[i] = binaryString.charCodeAt(i);
     }
     return bytes;
+}
+
+function prepareBase64ForDecode(b64) {
+    let unsafe = b64;
+    while (unsafe.length % 4 !== 0) {
+        unsafe += "=";
+    }
+    unsafe = unsafe
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+    return unsafe;
 }
 
 function zeroPad(text, size=8) {
